97d307f3e379ae118feec0bffb23b3258fd94fc7
Alexander Longum Olsen <alexanderlongumolsen@gmail.com>
2021-09-09 17:15:16 +0200

    Prosjektmappe template

---
+++ app/src/main/java/com/example/mob3000_gr3_app/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mob3000_gr3_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
